208c69bbf179cae97aad50ae7c54c0b69786918c
Tq-Lj <6du@protonmail.com>
2019-08-03 21:54:29 +0800

    .

---
+++ index.js
@@ -0,0 +1,65 @@
+(function(){
+  var path, fs, Config;
+  path = require('path');
+  fs = require('fs-extra');
+  Config = (function(){
+    Config.displayName = 'Config';
+    var prototype = Config.prototype, constructor = Config;
+    function Config(root){
+      this.root = root;
+      this.li = bind$(this, 'li', prototype);
+      this.line = bind$(this, 'line', prototype);
+    }
+    Config.prototype.line = async function(name, init){
+      var fpath, li, i$, ref$, len$, i;
+      fpath = path.join(this.root, name) + '.line.txt';
+      if (!(await fs.exists(fpath))) {
+        if (init) {
+          (await fs.outputFile(fpath, init));
+        }
+        return init;
+      }
+      li = (await fs.readFile(fpath, 'utf8'));
+      for (i$ = 0, len$ = (ref$ = li.split("\n")).length; i$ < len$; ++i$) {
+        i = ref$[i$];
+        i = i.trim();
+        if (i) {
+          if (i.charAt(0) !== "#") {
+            return i;
+          }
+        }
+      }
+      return init;
+    };
+    Config.prototype.li = async function(name, init){
+      var fpath, li, r, i$, ref$, len$, i;
+      fpath = path.join(this.root, name) + '.li.txt';
+      if (!(await fs.exists(fpath))) {
+        if (init.length) {
+          (await fs.outputFile(fpath, init.join('\n')));
+        }
+        return init;
+      }
+      li = (await fs.readFile(fpath, 'utf8'));
+      r = [];
+      for (i$ = 0, len$ = (ref$ = li.split("\n")).length; i$ < len$; ++i$) {
+        i = ref$[i$];
+        i = i.trim();
+        if (i) {
+          if (i.charAt(0) !== "#") {
+            r.push(i);
+          }
+        }
+      }
+      if (r.length) {
+        return r;
+      }
+      return init;
+    };
+    return Config;
+  }());
+  module.exports = Config;
+  function bind$(obj, key, target){
+    return function(){ return (target || obj)[key].apply(obj, arguments) };
+  }
+}).call(this);
---
+++ index.ls
@@ -0,0 +1,42 @@
+require! <[
+  path
+]>
+require! {
+  \fs-extra : fs
+}
+
+class Config
+  (@root) ->
+
+  line : (name, init)!~>
+    fpath = path.join(@root, name)+'.line.txt'
+    if not await fs.exists(fpath)
+      if init
+        await fs.outputFile(fpath, init)
+      return init
+    li = await fs.readFile(fpath, 'utf8')
+    for i in li.split("\n")
+      i = i.trim()
+      if i
+        if i.charAt(0) != "#"
+          return i
+    return init
+
+  li: (name, init)!~>
+    fpath = path.join(@root, name)+'.li.txt'
+    if not await fs.exists(fpath)
+      if init.length
+        await fs.outputFile(fpath, init.join('\n'))
+      return init
+    li = await fs.readFile(fpath, 'utf8')
+    r = []
+    for i in li.split("\n")
+      i = i.trim()
+      if i
+        if i.charAt(0) != "#"
+          r.push i
+    if r.length
+      return r
+    return init
+
+module.exports = Config
